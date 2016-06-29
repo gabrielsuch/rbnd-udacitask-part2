7bb84db0a5daa1c4c64f4a25c6007f8c361bd3c0
Gabriel Such <gabrielsuch@gmail.com>
2016-06-29 00:38:33 -0700

    foundation is working: add, delete, all and filter

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,18 +1,29 @@
+require 'chronic'
+
 class TodoItem
   # include Listable
   attr_reader :description, :due, :priority
 
+  @@valid_priorities = ["low", "medium", "high", nil]
+
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
+
+    if !@@valid_priorities.include? @priority
+      raise UdaciListErrors::InvalidPriorityValue.new("#{@priority} is not a valid priority")
+    end
   end
+
   def format_description
     "#{@description}".ljust(25)
   end
+
   def format_date
     @due ? @due.strftime("%D") : "No due date"
   end
+
   def format_priority
     value = " ⇧" if @priority == "high"
     value = " ⇨" if @priority == "medium"
@@ -20,9 +31,11 @@ class TodoItem
     value = "" if !@priority
     return value
   end
+
   def details
     format_description + "due: " +
     format_date +
     format_priority
   end
+
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -1,25 +1,49 @@
 class UdaciList
   attr_reader :title, :items
 
+  @@valid_types = [ "todo", "event", "link" ]
+
   def initialize(options={})
     @title = options[:title]
     @items = []
   end
+
   def add(type, description, options={})
     type = type.downcase
+    raise UdaciListErrors::InvalidItemType.new("#{type} is not a valid type") unless @@valid_types.include? type
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
+
   def delete(index)
+    raise UdaciListErrors::IndexExceedsListSize.new("list contains #{items.size} records") if index >= @items.size
     @items.delete_at(index - 1)
   end
+
   def all
-    puts "-" * @title.length
-    puts @title
-    puts "-" * @title.length
-    @items.each_with_index do |item, position|
+    display_title @title || 'Untitled List'
+    display_items @items
+  end
+
+  def filter(item_type)
+    type_class = { todo: TodoItem, event: EventItem, link: LinkItem }
+    items_type = @items.select { |item| item.class == type_class[item_type.to_sym] }
+
+    display_title "Items with type: #{item_type}"
+    display_items items_type
+  end
+
+  def display_title(title)
+    puts "-" * title.length
+    puts title
+    puts "-" * title.length
+  end
+
+  def display_items(items)
+    items.each_with_index do |item, position|
       puts "#{position + 1}) #{item.details}"
     end
   end
+
 end
